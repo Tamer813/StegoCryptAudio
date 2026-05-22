c8bc553497f928e0f76338fbae23748a7fa6af1e
Tamer813 <anonymousyemen484@gmail.com>
2026-05-23 00:09:14 +0300

    Remove app.run() from app.py
    Remove the main entry point for the application.

--- app.py
+++ app.py
@@ -93,5 +93,3 @@ def index():
 
     return render_template("index.html", message=message, download_link=download_link)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
